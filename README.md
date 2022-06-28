7e2f42acb8ee38a129fbf519df21119bf309e31a
leokim <kunga077@gmail.com>
2022-06-28 16:35:15 +0900

    docs: readme.md 추가

---
+++ README.md
@@ -0,0 +1,11 @@
+
+## 실행 방법
+
+```bash
+chmod +x howtoinstall.sh
+
+./howtoinstall.sh
+```
+
+localhost:1313
+
--- howtoinstall.sh
+++ howtoinstall.sh
@@ -1,5 +1,3 @@
-git clone https://github.com/morak-morak/hugo-blog.git
-
 git clone https://github.com/morak-morak/morak-morak.github.io.git public
 
 git clone https://github.com/adityatelange/hugo-PaperMod themes/PaperMod --depth=1
