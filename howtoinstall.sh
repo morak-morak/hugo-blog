f48e22809f0ee5a0cdeb461487f87fe88c9004f3
leokim <kunga077@gmail.com>
2022-06-28 16:20:48 +0900

    feat: how to install 추가

---
+++ howtoinstall.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/morak-morak/hugo-blog.git
+
+git clone https://github.com/morak-morak/morak-morak.github.io.git public
+
+git clone https://github.com/adityatelange/hugo-PaperMod themes/PaperMod --depth=1
+
+hugo server -D
+
+
